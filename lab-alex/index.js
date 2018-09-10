d20ef745c5bad585372ce5214742881fa26160ca
alexlhanson <alexlhanson@gmail.com>
2018-09-10 13:48:05 -0700

    Add constructor for BMT and CLI

--- lab-alex/index.js
+++ lab-alex/index.js
@@ -1,1 +1,30 @@
 'use strict';
+
+/********************************************************************************
+*         CLI                                                                   *
+********************************************************************************/
+let args = process.argv;
+
+if(args.length < 5){
+  throw new Error('--ERROR--\nusage: node index.js <input-file> <output-file> <transform-name>');
+}
+
+let inputFile = args[2];
+let outputFile = args[3];
+let transform = args[4];
+
+let message = `Using ${transform} on ${inputFile} and saving to ${outputFile}`;
+
+console.log(message);
+
+/********************************************************************************
+*         Operations                                                            *
+********************************************************************************/
+
+const BMT = require('./lib/bmt.js');
+let bmt = new BMT();
+
+bmt.open(inputFile, (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
