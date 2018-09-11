c407d8c1ffbfc089afc731367c7cfa06ce3aa04c
alexlhanson <alexlhanson@gmail.com>
2018-09-11 09:01:56 -0700

    Add invert transform and save to duck

--- lab-alex/lib/bmt.js
+++ lab-alex/lib/bmt.js
@@ -8,6 +8,7 @@ class BMT{
     this.infoHeaderBuf = null; 
     this.fileHeader = null;
     this.infoHeader = null;
+    this.body = null;
   }
   
   open(file, callback){
@@ -17,6 +18,7 @@ class BMT{
       this.buf = data;
       
       _parseBitMapBuffer(this);
+      _decodeBodyBuffer(this);
       
       callback(null, this);
     });
@@ -25,19 +27,23 @@ class BMT{
   invert(bitmap, callback){
     console.log('inverting');
     //transform colors
+    bitmap.invert = new Buffer.from(bitmap.body.map(decVal => 255 - parseInt(decVal)));
     callback(null, bitmap);
+    // console.log(bitmap.invert);
   }
 
   save(bitmap, outputFile, callback){
     //Write transformed butmap to disk
     //NOTE: transformed colors might still be encoded, they need to be raw.
     //NOTE: bitmap is NOT a Buffer, but fs.writeFile takes a buffer
-    console.log(bitmap);
-    
-    fs.writeFile(outputFile, this.buf, (err) =>{
+    // console.log(bitmap);
+    console.log(this.invert);
+    const transformed = Buffer.concat([bitmap.fileHeaderBuf, bitmap.infoHeaderBuf, bitmap.invert]);
+
+    fs.writeFile(outputFile, transformed, (err) =>{
       if (err) callback (err);
     });
-
+    
     callback(null);
   }
 }
@@ -50,7 +56,7 @@ let _parseBitMapBuffer = function(self){
     reserved: self.fileHeaderBuf.readUInt32LE(6),
     dataOffset: self.fileHeaderBuf.readUInt32LE(10),
   };
-  self.infoHeaderBuf = self.buf.slice(14, 30);
+  self.infoHeaderBuf = self.buf.slice(14, 54);
   self.infoHeader = {
     infoHeaderSize: self.infoHeaderBuf.readUInt32LE(0),
     widthPx: self.infoHeaderBuf.readUInt32LE(4),
@@ -58,7 +64,10 @@ let _parseBitMapBuffer = function(self){
     planes: self.infoHeaderBuf.readUInt16LE(12),
     bitPerPx: self.infoHeaderBuf.readUInt16LE(14),
   };
+  self.body = Array.from(self.buf.slice(54));
+};
 
+let _decodeBodyBuffer = function(self){
   
 };
 
